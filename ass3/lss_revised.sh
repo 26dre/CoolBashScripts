3e026423939dcbe6659b391e6c0155f89e6700be
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-02 15:34:40 -0800

    potential change for when num args = 0

--- ass3/lss_revised.sh
+++ ass3/lss_revised.sh
@@ -1,1 +1,8 @@
 #!/bin/bash                                                                                       
+
+long_bytes_position=5; 
+if [[$# -eq 0 ]]; then 
+    echo "Running LSS command with 0 arguments"
+    ls -l | sort -k$long_bytes_position -nr
+
+fi
