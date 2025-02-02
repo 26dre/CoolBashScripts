71d5b8e4eca488fe855f638cdf02daec91cdd339
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-02 15:37:10 -0800

    change made for comparison

--- ass3/lss_revised.sh
+++ ass3/lss_revised.sh
@@ -1,7 +1,7 @@
 #!/bin/bash                                                                                       
 
 long_bytes_position=5; 
-if [[$# -eq 0 ]]; then 
+if [-z "$#"]; then 
     echo "Running LSS command with 0 arguments"
     ls -l | sort -k$long_bytes_position -nr
 
