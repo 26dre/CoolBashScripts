f7949111e653697c7c0fd5f48a3adeec8350bebc
Andre Khmelnitsky <akhmelni@odin.ics.uci.edu>
2025-02-02 15:55:52 -0800

    reviseed lss script that concretely works for 0 inputs

--- ass3/lss_revised.sh
+++ ass3/lss_revised.sh
@@ -1,8 +1,10 @@
 #!/bin/bash                                                                                       
 
 long_bytes_position=5; 
-if [-z "$#"]; then 
+if [[ "$#" == 0 ]]; then 
     echo "Running LSS command with 0 arguments"
     ls -l | sort -k$long_bytes_position -nr
 
+else
+	echo "received $# arguments"
 fi
