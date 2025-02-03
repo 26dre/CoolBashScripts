fe982d31dd09e1b92cc8cd4ae55aace23ddd03db
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-02 23:43:45 -0800

    small change made

--- ass3/lss_revised.sh
+++ ass3/lss_revised.sh
@@ -53,7 +53,7 @@ for arg in $@; do
     unpack_arguments $arg
 done
 echo "Options list = ${options_list[@]}"
-shift $options_list
+shift options_list
 echo "Options list shifted = ${options_list[@]}"
 echo "Arguments list = ${file_args_list[@]}"
 
