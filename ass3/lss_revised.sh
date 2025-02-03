2211ef71e88cba25e08b3f582ff93d55e4ca542c
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-03 02:50:49 -0800

    small change made

--- ass3/lss_revised.sh
+++ ass3/lss_revised.sh
@@ -142,8 +142,7 @@ fi
 # if contains -r ${ls_options[@]};
 final_ls_cmd="$base_ls_cmd ${ls_options[@]} ${file_args_list[@]}" 
 final_tail_cmd="$tail_cmd"  
-final_sort_cmd=$sort -nk$long_bytes_position
-
+base_sort_cmd="sort -nk$long_bytes_position";
 
 if contains -h ${ls_options[@]}; then
     # echo "Requires humanity"
