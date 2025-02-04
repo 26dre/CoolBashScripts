47d9924fa729f82343f26d00f874405a791b9263
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-04 05:28:48 -0800

    making some addl changes for testing

--- ass3/SafeDeleteSuite/lsrm.sh
+++ ass3/SafeDeleteSuite/lsrm.sh
@@ -22,4 +22,11 @@ ls_trash () {
     done  
 }
 
-ls_trash
+if [[ -d $TRASH ]]; then
+    ls_trash
+    exit 0
+else
+    echo "Could not find Trash. Check you trash directory to make sure that it is set up and that TRASH variable is set up"
+    exit 1
+fi
+
