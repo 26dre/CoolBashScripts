88887c3716fec1fcab480cad49380e3ef82463c6
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-04 05:26:08 -0800

    making some addl changes for testing

--- ass3/SafeDeleteSuite/durm.sh
+++ ass3/SafeDeleteSuite/durm.sh
@@ -1,7 +1,26 @@
+du_trash () {
+    for file in "$TRASH"/*; do
+        [ -e "$file" ] || continue  
+
+        # Extract original filename by decoding base64
+        
+        decoded_name=$(basename "$file" | base64 --decode 2>/dev/null)
+
+        file_info=$(du "$file" | awk '{for(i=1; i<NF; i++) printf $i " "; }')
+
+        printf "%s%s\n" "$file_info" "$decoded_name"
+
+    done  
+}
+
+ls_trash
+
 if [[ -d $TRASH ]]; then
-    du -h "${TRASH:?}"/*
+    du_trash 
     exit 0
 else
     echo "Could not find Trash. Check you trash directory to make sure that it is set up and that TRASH variable is set up"
     exit 1
 fi
+
+
