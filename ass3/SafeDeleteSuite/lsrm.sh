6856f5b5b2037d054d003f66f9d2d010d1ea1ef5
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-04 05:29:42 -0800

    should be done soon

--- ass3/SafeDeleteSuite/lsrm.sh
+++ ass3/SafeDeleteSuite/lsrm.sh
@@ -1,11 +1,4 @@
 #!/bin/bash                                                                                       
-# if [[ -d $TRASH ]]; then
-#     ls -lR "$TRASH"
-#     exit 0
-# else
-#     echo "Could not find Trash. Check you trash directory to make sure that it is set up and that TRASH variable is set up"
-#     exit 1
-# fi
 
 ls_trash () {
     for file in "$TRASH"/*; do
