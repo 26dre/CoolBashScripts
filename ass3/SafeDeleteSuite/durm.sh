36863850a18341de235fdc57699d0334e825e5e4
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-04 03:48:33 -0800

    making some addl changes for testing

--- ass3/SafeDeleteSuite/durm.sh
+++ ass3/SafeDeleteSuite/durm.sh
@@ -1,5 +1,5 @@
 if [[ -d $TRASH ]]; then
-    du -sh "${TRASH:?}"/*
+    du -h "${TRASH:?}"/*
     exit 0
 else
     echo "Could not find Trash. Check you trash directory to make sure that it is set up and that TRASH variable is set up"
