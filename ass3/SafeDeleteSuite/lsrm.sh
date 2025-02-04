74949dbc8a4a45ba7a47ee24b7eb8a7613ac5f6a
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-04 04:35:07 -0800

    making some addl changes for testing

--- ass3/SafeDeleteSuite/lsrm.sh
+++ ass3/SafeDeleteSuite/lsrm.sh
@@ -1,8 +1,25 @@
 #!/bin/bash                                                                                       
-if [[ -d $TRASH ]]; then
-    ls -lR "$TRASH"
-    exit 0
-else
-    echo "Could not find Trash. Check you trash directory to make sure that it is set up and that TRASH variable is set up"
-    exit 1
-fi
+# if [[ -d $TRASH ]]; then
+#     ls -lR "$TRASH"
+#     exit 0
+# else
+#     echo "Could not find Trash. Check you trash directory to make sure that it is set up and that TRASH variable is set up"
+#     exit 1
+# fi
+
+ls_trash() {
+    for file in "$TRASH"/*; do
+        [ -e "$file" ] || continue  # Skip if no files exist
+
+        # Extract original filename by decoding base64
+        decoded_name=$(basename "$file" | base64 --decode 2>/dev/null)
+
+        # Use ls -l to get file details
+        file_info=$(ls -l "$file")
+
+        # Print ls output with decoded filename
+        printf "%s -> %s\n" "$file_info" "$decoded_name"
+    done
+}
+
+ls_trash
