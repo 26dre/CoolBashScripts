3c02159167d25ced1a93e423417b8d061725a50d
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-04 01:08:55 -0800

    making some addl changes for testing

--- ass3/SafeDeleteSuite/srm.sh
+++ ass3/SafeDeleteSuite/srm.sh
@@ -110,7 +110,7 @@ get_files_to_delete () {
         files_to_delete+=("${tmp_files_to_delete[@]}")
     done
 }
-# handle_empty_trash_variable
+handle_empty_trash_variable
 echo "${file_args[@]}"
 get_files_to_delete "${file_args[@]}"
 echo "List of files to delete = ${files_to_delete[*]}"
