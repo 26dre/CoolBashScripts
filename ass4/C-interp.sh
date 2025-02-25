c5c66d079e34aa415248ac0d217b6f74fe8515cf
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-24 18:03:37 -0800

    c-interp is finished this will allow you to link and run c as if it were a interpreted language that rpdocued no object files (for once)

---
+++ ass4/C-interp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function cleanup()
+{
+    rm -rf "${tmp_dir}" 
+    trap - SIGHUP SIGINT SIGQUIT SIGTERM 
+}
+
+script_name=$(basename "$0")
+c_file="${script_name}.c"
+out_file="${script_name}.out"
+
+tmp_dir="/tmp/awkawk3000"
+if [ ! -d "$tmp_dir" ]; then 
+    mkdir "$tmp_dir"
+fi
+
+
+gcc "$c_file" -o "${tmp_dir}/${out_file}"
+trap cleanup 0 1 2 3 15
+"${tmp_dir}/${out_file}" "$@"
+cleanup
+
+
+
+
+
+
+
+
