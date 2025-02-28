89081d5ddb318d454eba83f02d335b29773fed44
Andre Khmelnitsky <akhmelni@uci.edu>
2025-02-28 03:32:59 -0800

    added a file that should fully work as C interpreter as was on my openllab

---
+++ ass4/Cinterp_comp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function cleanup()
+{
+    rm -rf "${tmp_dir}" 
+    trap - 0 1 2 3 15 
+}
+chmod +x "$0"
+script_name=$(basename "$0")
+c_file="${script_name}.c"
+out_file="${script_name}.out"
+
+tmp_dir="/tmp/awkawk3000"
+if [ ! -d "$tmp_dir" ]; then 
+    mkdir "$tmp_dir"
+fi
+
+
+gcc "$c_file" -o "${tmp_dir}/${out_file}"
+trap cleanup 0 1 2 3 15
+"${tmp_dir}/${out_file}" "$@"
+cleanup
+
