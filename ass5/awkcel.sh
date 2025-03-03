b8df025220fe4686eb686ba4889971fee360528f
Andre Khmelnitsky <akhmelni@uci.edu>
2025-03-03 00:55:46 -0800

    added some extra logging for debugging purposes

--- ass5/awkcel.sh
+++ ass5/awkcel.sh
@@ -43,6 +43,7 @@ BEGIN {
     in_quotes = 0;
     n = split($0, tokens, /([[:space:]]+|[^[:alnum:]_]+)/, seps);  
 
+    printf "Tokens = ", tokens > "dev/stderr";
     for (i = 1; i <= n; i++) {
         if (tokens[i] ~ /^"/) in_quotes = !in_quotes;  
 
