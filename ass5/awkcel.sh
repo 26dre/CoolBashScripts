060828386d903dbd1dbfb4011a2f30f7e5014013
Andre Khmelnitsky <akhmelni@uci.edu>
2025-03-03 21:39:59 -0800

    fuckme

--- ass5/awkcel.sh
+++ ass5/awkcel.sh
@@ -123,7 +123,9 @@ FULL_CMD=$(echo "-F'$DELIMITER'" "'$SKIP_AWK_PROG'" "$FILENAME")
     echo "FULL_CMD = $FULL_CMD"
     echo "WANTED = ""-F'$DELIMITER'" "'$SKIP_AWK_PROG'" "$FILENAME"
 } >&2
+set -x
 awk "$FULL_CMD"
+set +x 
 # awk  -F'\t' 'NR == 1 { next } $2=="00000"{print $1, "is the prof"} $2!="00000"{ print $1, "must be a TA or Reader or Student and got grade", int($3+0.5)}' name-studnum.tsv
 
 
