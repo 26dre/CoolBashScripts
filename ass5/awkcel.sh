b716a92b56641caf35ba6ffcc691566706a17975
Andre Khmelnitsky <akhmelni@uci.edu>
2025-03-03 20:29:17 -0800

    removed unnecessary

--- ass5/awkcel.sh
+++ ass5/awkcel.sh
@@ -107,18 +107,12 @@ BEGIN {
     printf "\n";
 }')
 
-    echo "Pre processed awk program: $AWK_PROGRAM"
+    # echo "Pre processed awk program: $AWK_PROGRAM"
 
 }
 
 interpret_first_line
-echo "Variable declarations $AWK_VAR_DECLS"
-AWK_CMD_TO_RUN="-F'$DELIMITER' 'NR == 1 { next } $AWK_PROGRAM' $FILENAME"
-echo "$AWK_CMD_TO_RUN"
 # awk "-F'$DELIMITER' 'NR == 1 { next } $AWK_PROGRAM' $FILENAME"
-#  awk -F'\t' 'NR > 1 {$2 == "00000" { print $1, "is the prof" } 
-# $2 != "00000" { print $1, "must be a TA or Reader or Student and got grade", int($3 + 0.5) }}' name-studnum.tsv
-# # AWK_CMD_TO_RUN
 
 
 # working command
