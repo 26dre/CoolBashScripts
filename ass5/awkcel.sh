d67ab7c98bbb0de63543b1a0513664ce55ecaf82
Andre Khmelnitsky <akhmelni@uci.edu>
2025-03-03 21:30:15 -0800

    wait

--- ass5/awkcel.sh
+++ ass5/awkcel.sh
@@ -121,8 +121,8 @@ SKIP_AWK_PROG="NR == 1 { next } $AWK_PROGRAM"
     echo EXCLUDING_FILE_NAME = "$EXCLUDING_FILE_NAME" 
     echo "FULL_CMD == " "-F'$DELIMITER'" "'$SKIP_AWK_PROG'" "$FILENAME"
 } >&2
-awk -F'$DELIMITER' '$SKIP_AWK_PROG' "$FILENAME"
-awk  -F'\t' 'NR == 1 { next } $2=="00000"{print $1, "is the prof"} $2!="00000"{ print $1, "must be a TA or Reader or Student and got grade", int($3+0.5)}' name-studnum.tsv
+awk -F'$DELIMITER' "'$SKIP_AWK_PROG'" "$FILENAME"
+# awk  -F'\t' 'NR == 1 { next } $2=="00000"{print $1, "is the prof"} $2!="00000"{ print $1, "must be a TA or Reader or Student and got grade", int($3+0.5)}' name-studnum.tsv
 
 
 # working command
