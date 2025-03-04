b039d25cb64a28791eaf779751bc61a3f731a731
Andre Khmelnitsky <akhmelni@uci.edu>
2025-03-03 20:33:03 -0800

    im cook

--- ass5/awkcel.sh
+++ ass5/awkcel.sh
@@ -112,6 +112,8 @@ BEGIN {
 }
 
 interpret_first_line
+FULL_AWK_CMD="-F'$DELIMITER' 'NR == 1 { next } $AWK_PROGRAM' $FILENAME"
+echo "$FULL_AWK_CMD"
 # awk "-F'$DELIMITER' 'NR == 1 { next } $AWK_PROGRAM' $FILENAME"
 
 
