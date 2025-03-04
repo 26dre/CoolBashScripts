b786242f4be730782cc9efc4d27032140a6163b1
Andre Khmelnitsky <akhmelni@uci.edu>
2025-03-04 14:22:14 -0800

    removed debugging from file

--- ass5/awkcel.sh
+++ ass5/awkcel.sh
@@ -130,10 +130,8 @@ FULL_CMD=$(echo "-F'$DELIMITER'" "'$SKIP_AWK_PROG'" "$FILENAME")
 } >&2
 
 
-set -x
 # awk "$FULL_CMD"
 awk -F"$DELIMITER" "$SKIP_AWK_PROG" "$FILENAME"
-set +x 
 # awk  -F'\t' 'NR == 1 { next } $2=="00000"{print $1, "is the prof"} $2!="00000"{ print $1, "must be a TA or Reader or Student and got grade", int($3+0.5)}' name-studnum.tsv
 
 
