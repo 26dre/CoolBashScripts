cd01e2f45ee1ce52eb14a55cd3a12bf2b74eaa2c
Andre Khmelnitsky <akhmelni@uci.edu>
2025-03-03 20:58:13 -0800

    small error fix hopefully

--- ass5/awkcel.sh
+++ ass5/awkcel.sh
@@ -113,10 +113,10 @@ BEGIN {
 }
 
 interpret_first_line
-FULL_AWK_CMD="-F'$DELIMITER' 'NR == 1 { next } $AWK_PROGRAM' $FILENAME"
+FULL_AWK_CMD=-F'$DELIMITER' 'NR == 1 { next } $AWK_PROGRAM' "$FILENAME"
 echo "$FULL_AWK_CMD" >&2
 # awk "$FULL_AWK_CMD"
-awk -F'$DELIMITER' 'NR == 1 { next } $AWK_PROGRAM' "$FILENAME"
+awk $FULL_AWK_CMD
 
 
 # working command
